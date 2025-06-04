6a1fe050d5554dc7afe84ddd2865921f9ce5d155
vikas wankhede <vikasw@marketamerica.com>
2025-06-04 22:55:35 +0530

    new react app

--- src/interfaces/User.ts
+++
@@ -1,9 +0,0 @@
-export type UserRole = 'admin' | 'user';
-
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-  role: UserRole;
-  token?: string;
-}
--- src/types/board.tsx
+++
@@ -1,7 +0,0 @@
-export type Board = {
-  id: number;
-  name: string;
-  ownerId: number;
-  createdAt: string;
-  updatedAt: string;
-};
